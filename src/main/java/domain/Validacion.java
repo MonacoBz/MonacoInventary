d7aafb032f9099c51198df87c37aaa779b9ba356
MonacoBz <gerardossj2@hotmail.com>
2025-09-11 18:55:31 -0600

    Se creo clase Validacion

---
+++ src/main/java/domain/Validacion.java
@@ -0,0 +1,25 @@
+package domain;
+
+import domain.dto.ProductoDto;
+
+import java.math.BigDecimal;
+
+public class Validacion {
+
+    private static boolean validarNombre(String nombre){
+        return !nombre.matches("^[A-Za-zÁÉÍÓÚáéíóúÑñ\\s]+$");
+    }
+
+    private static boolean validarPrecio(BigDecimal precio){
+        return precio.compareTo(BigDecimal.ZERO) <= 0;
+    }
+
+    private static boolean validarStock(Long stock){
+        return stock <= 0;
+    }
+
+    public static boolean validarProducto(ProductoDto p){
+        return validarNombre(p.nombre()) && validarPrecio(p.precio()) && validarStock(p.stock());
+    }
+
+}
